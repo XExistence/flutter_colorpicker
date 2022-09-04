a1d11e1e40dcd2141d5d812285f2ae0e442df4a9
Siya <SKubeka@sybrin.co.za>
2022-09-04 12:56:49 +0200

    WIP

--- lib/src/material_picker.dart
+++ lib/src/material_picker.dart
@@ -31,26 +31,25 @@ class MaterialPicker extends StatefulWidget {
 
 class _MaterialPickerState extends State<MaterialPicker> {
   final List<List<Color>> _colorTypes = [
-    [Colors.red, Colors.redAccent],
-    [Colors.pink, Colors.pinkAccent],
-    [Colors.purple, Colors.purpleAccent],
-    [Colors.deepPurple, Colors.deepPurpleAccent],
-    [Colors.indigo, Colors.indigoAccent],
-    [Colors.blue, Colors.blueAccent],
-    [Colors.lightBlue, Colors.lightBlueAccent],
-    [Colors.cyan, Colors.cyanAccent],
-    [Colors.teal, Colors.tealAccent],
-    [Colors.green, Colors.greenAccent],
-    [Colors.lightGreen, Colors.lightGreenAccent],
-    [Colors.lime, Colors.limeAccent],
-    [Colors.yellow, Colors.yellowAccent],
-    [Colors.amber, Colors.amberAccent],
-    [Colors.orange, Colors.orangeAccent],
-    [Colors.deepOrange, Colors.deepOrangeAccent],
+    [Colors.red],
+    [Colors.pink],
+    [Colors.purple],
+    [Colors.deepPurple],
+    [Colors.indigo],
+    [Colors.blue],
+    [Colors.lightBlue],
+    [Colors.cyan],
+    [Colors.teal],
+    [Colors.green],
+    [Colors.lightGreen],
+    [Colors.lime],
+    [Colors.yellow],
+    [Colors.amber],
+    [Colors.orange],
+    [Colors.deepOrange],
     [Colors.brown],
     [Colors.grey],
     [Colors.blueGrey],
-    [Colors.black],
   ];
 
   List<Color> _currentColorType = [Colors.red, Colors.redAccent];
@@ -60,32 +59,10 @@ class _MaterialPickerState extends State<MaterialPicker> {
     List<Map<Color, String>> result = [];
 
     for (Color colorType in colors) {
-      if (colorType == Colors.grey) {
-        result.addAll([
-          50,
-          100,
-          200,
-          300,
-          350,
-          400,
-          500,
-          600,
-          700,
-          800,
-          850,
-          900
-        ].map((int shade) => {Colors.grey[shade]!: shade.toString()}).toList());
-      } else if (colorType == Colors.black || colorType == Colors.white) {
-        result.addAll([
-          {Colors.black: ''},
-          {Colors.white: ''}
-        ]);
-      } else if (colorType is MaterialColor) {
+      if (colorType is MaterialColor) {
         result.addAll([50, 100, 200, 300, 400, 500, 600, 700, 800, 900]
             .map((int shade) => {colorType[shade]!: shade.toString()})
             .toList());
-      } else {
-        result.add({const Color(0x00000000): ''});
       }
     }
 
